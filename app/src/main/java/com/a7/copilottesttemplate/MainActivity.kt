ccd71621f103bdac32d2176d794f5a059332c8a5
A7ak <51486750+A7ak@users.noreply.github.com>
2026-03-12 00:10:52 +0530

    test 2 updating single comment changes

--- app/src/main/java/com/a7/copilottesttemplate/MainActivity.kt
+++ app/src/main/java/com/a7/copilottesttemplate/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.a7.copilottesttemplate.ui.theme.CopilotTestTemplateTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,12 +27,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             CopilotTestTemplateTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    var count by remember { mutableStateOf(0) }
+    
+    Column(modifier = modifier.padding(16.dp)) {
+        Greeting(name = "Android")
+        Text(text = "You clicked the button $count times", modifier = Modifier.padding(top = 8.dp))
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text(text = "Click me!")
         }
     }
 }
@@ -42,6 +62,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     CopilotTestTemplateTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
