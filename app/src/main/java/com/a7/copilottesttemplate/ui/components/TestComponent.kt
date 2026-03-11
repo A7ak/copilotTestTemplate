0cc4b100035024662f33f03cdb46ece36935b152
A7ak <51486750+A7ak@users.noreply.github.com>
2026-03-12 00:07:40 +0530

    test changes single template

---
+++ app/src/main/java/com/a7/copilottesttemplate/ui/components/TestComponent.kt
@@ -0,0 +1,9 @@
+package com.a7.copilottesttemplate.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TestComponent() {
+    Text(text = "This is a test component")
+}
